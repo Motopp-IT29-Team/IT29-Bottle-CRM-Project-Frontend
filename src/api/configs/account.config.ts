c055f09a58a47f8e8d8836bb7061027a7442fd8d
Nurullah K <nurullahkaratas@gmail.com>
2026-01-06 08:44:03 +0100

    feat: improve account phone field with user-friendly phone input component

--- src/api/configs/account.config.ts
+++ src/api/configs/account.config.ts
@@ -48,8 +48,8 @@ export const getAccountConfig = (params: GetAccountConfigParams = {}): IFormConf
                     {
                         name: 'phone',
                         label: 'Phone',
-                        type: 'text',
-                        placeholder: 'Enter phone number',
+                        type: 'phone',
+                        placeholder: '',
                     },
                     {
                         name: 'website',
